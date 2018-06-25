ed40410dce9f902f14fe5e0d32626ad6757a7fe8
thebreezyp <patrickfinnigan94@gmail.com>
2018-06-24 22:01:00 -0700

    minor work on html and js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,33 @@
-$(document).ready(()=>{
-
-
-$("form").submit((event)=>{
-    event.preventDefaul()
-    console.log(event.target[0].value);
-})
 
 
 
+$("#submit").on("click", function() {
+    event.preventDefault();
+
+    var animalValue = $("#animalEntry").val().trim();
+    console.log(animalValue);
+
+    $("animalEntry").val('')
+
+
+});
+
+
+
+
+
+
+var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC"
+
+$.ajax({
+    // ajax is used to acquire the 
+    url: queryURL,
+    method: "GET"
+}).then(function(response) {
+    console.log(response);
+    var results = response.data;
+    $("#").on("click", function() {
+    
+    
+    });
 });
